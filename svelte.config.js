d71b67ad1acb20183510acd8de1f4af90ba29ac1
PJ Molski <pjmolski@gmail.com>
2026-07-28 16:35:54 -0400

    Update svelte.config.js to nodejs 24.x

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
 
@@ -13,7 +13,8 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter({
-			maxDuration: 300
+			maxDuration: 300,
+			runtime: 'nodejs24.x'
 		}),
 		alias: {
 			$root: path.resolve('.')
