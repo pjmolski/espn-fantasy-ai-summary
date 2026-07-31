28c751e3895ee6c496cab44c4de8a68a5495333d
PJ Molski <pmolski@cordial.com>
2026-07-31 14:11:53 -0400

    feat: add Google Analytics (G-45H05PFT8J) to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
 	import '../app.css';
+	import { inject } from '@vercel/analytics';
+	inject();
 </script>
+
+<svelte:head>
+	<!-- Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-45H05PFT8J"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+		gtag('config', 'G-45H05PFT8J');
+	</script>
+</svelte:head>
 
 <slot />
